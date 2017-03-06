e2daa0a844b543a54c54fa38023a54543157fbf8
铭壬 <boyang.lby@alibaba-inc.com>
2017-03-06 15:26:08 +0800

    init project

---
+++ src/main/java/function/FunctionalInteferface.java
@@ -0,0 +1,15 @@
+package function;
+
+/**
+ * @author boyang.lby
+ * @date 3/6/17
+ * @description
+ */
+//@FunctionalInterface
+public interface FunctionalInteferface {
+
+
+    void apply();
+
+    void apply1();
+}
